50dcdfadd40e98fc17eb6a1247b1ca52ac5f804b
dembasow98 <dastech1998@gmail.com>
2022-09-19 17:41:18 +0300

    Done with the sanity setups and models creation. Backend part done.

---
+++ backend/schemas/save.js
@@ -0,0 +1,18 @@
+export default{
+
+    name: 'save',
+    title: 'Save',
+    type: 'document',
+    fields: [
+        {
+            name: 'userID',
+            title: 'UserId',
+            type: 'string',
+        },
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'postedBy',
+        },
+    ]
+}
